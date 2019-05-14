4df3ca24242ea2f2bf72fa5301c37674bb918da8
sir-dio <dubrovin.io@icloud.com>
2019-05-14 16:31:57 +0300

    initialized the network package

---
+++ qbnetwork/client.py
@@ -0,0 +1,9 @@
+""" qBarista: tasty web-served seismic tests.
+
+This file defines the socket-client that is used on the main computer to
+control the Raspberry Pi.
+
+@author: sir-dio
+e-mail: dubrovin.io@icloud.com
+
+"""
---
+++ qbnetwork/server.py
@@ -0,0 +1,9 @@
+""" qBarista: tasty web-served seismic tests.
+
+This file defines the socket-server that runs on the Raspberry Pi and is used
+to accept commands via the main computer based socket-clie.
+
+@author: sir-dio
+e-mail: dubrovin.io@icloud.com
+
+"""
