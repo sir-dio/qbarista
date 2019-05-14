84c7d4a010fe459f4f7e1465434216b84aee2c74
sir-dio <dubrovin.io@icloud.com>
2019-05-14 16:33:21 +0300

    initialized the webapp package

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,3 +1,12 @@
+""" qBarista: tasty web-served seismic tests.
+
+This file initializes the web application.
+
+@author: sir-dio
+e-mail: dubrovin.io@icloud.com
+
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,3 +1,12 @@
+""" qBarista: tasty web-served seismic tests.
+
+This file defines the roots that are used for the web application.
+
+@author: sir-dio
+e-mail: dubrovin.io@icloud.com
+
+"""
+
 from webapp import app
 
 
