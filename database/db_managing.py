dfe9014eef1af4c996927e87761e461ecdc6c271
sir-dio <dubrovin.io@icloud.com>
2019-05-14 18:14:51 +0300

    initialize the database

---
+++ database/db_managing.py
@@ -0,0 +1,10 @@
+""" qBarista: tasty web-served seismic tests.
+
+This file implements the database management functionality for the app.
+
+@author: sir-dio
+e-mail: dubrovin.io@icloud.com
+
+"""
+
+import sqlite3
