ac3b6360bd0798ff3d3f7690ad56f640344cc5bd
sir-dio <dubrovin.io@icloud.com>
2019-05-16 18:37:00 +0300

    added class to represent questions in the database

--- database/models.py
+++ database/models.py
@@ -18,3 +18,38 @@ class Test:
 
     def load_from_database(self):
         pass
+
+
+class Question:
+    """ A class representing a question for the quiz. """
+
+    def __init__(self, interactive=True):
+        self.id = None
+        self.question = None
+        self.correct = []
+        self.incorrect = []
+        self.quiz_id = None
+
+        if interactive:
+            self.fill_interactively()
+
+    def fill_interactively(self):
+        """ Create a question interactively. """
+
+        self.question = input('Вопрос: ')
+
+        self.correct.append(input('Правильный ответ: '))
+        for i in range(1, 4):
+            additional = input('%sй дополнительный правильный ответ: ' % i)
+            if not additional:
+                break
+            self.correct.append(additional)
+
+        self.incorrect.append(input('1й неправильный ответ: '))
+        for i in range(2, 8):
+            additional = input('%sй неправильный ответ: ' % i)
+            if not additional:
+                break
+            self.incorrect.append(additional)
+
+        self.quiz_id = input('ID теста, к которому относится вопрос: ')
