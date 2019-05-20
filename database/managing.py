e7819444cd6a598412a9385706cf1d326711c5f8
sir-dio <dubrovin.io@icloud.com>
2019-05-20 15:26:41 +0300

    working prototype of question model

--- database/managing.py
+++ database/managing.py
@@ -19,36 +19,30 @@ def initialize():
         cursor = connection.cursor()
 
         # create missing tables
-        cursor.execute("""CREATE TABLE IF NOT EXISTS quizzes(
-                            id TEXT PRIMARY KEY,
-                            name TEXT NOT NULL,
-                            var INTEGER NOT NULL
-                            );""")
-
         cursor.execute("""CREATE TABLE IF NOT EXISTS questions(
-                            id INTEGER PRIMARY KEY, 
-                            question TEXT NOT NULL UNIQUE,
+                            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                            question TEXT NOT NULL,                            
                             correct1 TEXT NOT NULL,
-                            correct2 TEXT NOT NULL,
-                            correct3 TEXT NOT NULL,
-                            correct4 TEXT NOT NULL,
+                            correct2 TEXT,
+                            correct3 TEXT,
+                            correct4 TEXT,
                             incorrect1 TEXT NOT NULL,
-                            incorrect2 TEXT NOT NULL,
-                            incorrect3 TEXT NOT NULL,
-                            incorrect4 TEXT NOT NULL,
-                            incorrect5 TEXT NOT NULL,
-                            incorrect6 TEXT NOT NULL,
-                            incorrect7 TEXT NOT NULL,
-                            quiz_id TEXT NOT NULL,
-                            FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
+                            incorrect2 TEXT,
+                            incorrect3 TEXT,
+                            incorrect4 TEXT,
+                            incorrect5 TEXT,
+                            incorrect6 TEXT,
+                            incorrect7 TEXT,
+                            picture TEXT,
+                            quiz_id TEXT NOT NULL
                             );""")
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS results(
-                            id INTEGER PRIMARY KEY,
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
                             student TEXT NOT NULL,
                             score INTEGER NOT NULL,
-                            quiz TEXT NOT NULL,
-                            FOREIGN KEY (quiz) REFERENCES quizzes(name)
+                            quiz_id TEXT NOT NULL,
+                            FOREIGN KEY (quiz_id) REFERENCES quizzes(quiz_id)
                             );""")
 
 
@@ -70,13 +64,35 @@ def drop_all(force=False):
     connection = sqlite3.connect('alex.db')
 
     with connection:
-        c = connection.cursor()
+        cursor = connection.cursor()
 
-        c.execute('DROP TABLE IF EXISTS answers;')
-        c.execute('DROP TABLE IF EXISTS quizzes;')
-        c.execute('DROP TABLE IF EXISTS results;')
+        cursor.execute('DROP TABLE IF EXISTS questions;')
+        cursor.execute('DROP TABLE IF EXISTS results;')
 
 
-if __name__ == '__main__':
-    drop_all()
-    initialize()
+def add_question(question):
+    """ Adds a question to the database. """
+
+    connection = sqlite3.connect('database/alex.db')
+
+    query = """INSERT INTO questions (question, correct1, correct2, correct3, correct4,
+                       incorrect1, incorrect2, incorrect3, incorrect4,
+                       incorrect5, incorrect6, incorrect7, picture, quiz_id)
+                VALUES (:q, :c1, :c2, :c3, :c4, :i1, :i2, :i3, :i4, :i5, :i6, :i7,
+                        :pic, :id); """
+
+    values = {
+        'q': question.question,
+        'pic': question.picture,
+        'id': question.quiz_id
+    }
+
+    for i, ans in enumerate(question.correct, 1):
+        values['c%i' % i] = ans
+
+    for i, ans in enumerate(question.incorrect, 1):
+        values['i%i' % i] = ans
+
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(query, values)
--- database/models.py
+++ database/models.py
@@ -7,27 +7,17 @@ e-mail: dubrovin.io@icloud.com
 
 """
 
-
-class Test:
-    """ A class representing a quiz. """
-
-    def __init__(self, id=None, name=None, var=None):
-        self.id = id
-        self.name = name
-        self.var = var
-
-    def load_from_database(self):
-        pass
+import random
 
 
 class Question:
     """ A class representing a question for the quiz. """
 
-    def __init__(self, interactive=True):
-        self.id = None
+    def __init__(self, interactive=False):
         self.question = None
         self.correct = []
         self.incorrect = []
+        self.picture = None
         self.quiz_id = None
 
         if interactive:
@@ -36,20 +26,55 @@ class Question:
     def fill_interactively(self):
         """ Create a question interactively. """
 
+        # the question itself
         self.question = input('Вопрос: ')
 
+        # first correct answer
         self.correct.append(input('Правильный ответ: '))
+
+        # up to 3 additional correct answers
         for i in range(1, 4):
             additional = input('%sй дополнительный правильный ответ: ' % i)
+
+            # if empty, fill the rest with None
             if not additional:
+                for j in range(i, 4):
+                    self.correct.append(None)
                 break
+
             self.correct.append(additional)
 
+        # first incorrect answer
         self.incorrect.append(input('1й неправильный ответ: '))
+
+        # up to 6 additional incorrect answers:
         for i in range(2, 8):
             additional = input('%sй неправильный ответ: ' % i)
+
+            # again, if empty, fill the rest with None
             if not additional:
+                for j in range(i, 8):
+                    self.incorrect.append(None)
                 break
+
             self.incorrect.append(additional)
 
+        # ID of the quiz
         self.quiz_id = input('ID теста, к которому относится вопрос: ')
+
+    def add_picture(self, picture):
+        """ Adds a picture to the question. """
+        self.picture = picture
+
+    @property
+    def answers(self):
+        return [answer for answer in self.correct + self.incorrect if answer]
+
+    def __str__(self):
+        repr = '%s\n' % self.question
+        for answer in random.sample(self.answers, len(self.answers)):
+            repr += '\t* %s\n' % answer
+        return repr
+
+    def __repr__(self):
+        return '%s\n' % self.question
